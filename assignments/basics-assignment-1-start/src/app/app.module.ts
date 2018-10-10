30e2a93da3b048d7bf05ea9050f7fb7c10ba0a76
Ramakrishnan S <ramakrishnan.sathyanarayanan@solitontech.com>
2018-10-10 18:19:17 +0530

    first assign

---
+++ assignments/basics-assignment-1-start/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { WarningsComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WarningsComponent,
+    SuccessComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
